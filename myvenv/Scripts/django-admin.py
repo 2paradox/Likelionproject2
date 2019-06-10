d141dc06eb854e3acd1b7724eff290c2b72ea201
2paradox <dltjdcjf93@pusan.ac.kr>
2019-06-10 15:59:22 +0900

    wordcount

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\lee seong cheol\desktop\멋사\멋사과제\wordcounter\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
